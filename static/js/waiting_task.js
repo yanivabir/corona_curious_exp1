a287e4668b9edbf145f9192d713528fca40fb3f0
yanivabir <yanivabir@gmail.com>
2020-03-10 20:56:51 -0400

    Update waiting_task.js

--- static/js/waiting_task.js
+++ static/js/waiting_task.js
@@ -4,7 +4,7 @@
 var maxStimDuration = 10000,
   minResponseTime = 1500,
   tooSlowTime = 1000,
-  maxAnswerTime = 7,
+  maxAnswerTime = 7000,
   postTooSlowTime = 800,
   fixationTime = 500,
   maxTaskTime = 2.5,
